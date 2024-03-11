aeca31581a3e43ee05b73db7d0f8fba7072247d3
Udo Bauer <knackwurstking.tux@gmail.com>
2024-03-11 18:29:06 +0100

    move interactive (terminal) cli stuff to internal/shell

--- cmd/picow-led/main.go
+++ cmd/picow-led/main.go
@@ -1,53 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
-	"github.com/c-bata/go-prompt"
-	"golang.org/x/term"
+	shell "github.com/knackwurstking/picow-led/internal/shell"
 )
-
-var (
-	termState *term.State
-)
-
-func saveTermState() {
-	oldState, err := term.GetState(int(os.Stdin.Fd()))
-	if err != nil {
-		return
-	}
-	termState = oldState
-}
-
-func restoreTermState() {
-	if termState != nil {
-		term.Restore(int(os.Stdin.Fd()), termState)
-	}
-}
-
-func exit() {
-	restoreTermState()
-	os.Exit(0)
-}
 
 func main() {
-	defer restoreTermState()
-	saveTermState()
-
-	for {
-		t := prompt.Input(
-			"[picow] ",
-			completer,
-			prompt.OptionPrefixTextColor(prompt.Blue),
-		)
-
-		switch strings.Trim(t, " ") {
-		case "exit", "quit":
-			exit()
-		}
-
-		fmt.Printf("Run command: %s\n", t)
-	}
+	shell.Run()
 }
---
+++ internal/shell/shell.go
@@ -0,0 +1,53 @@
+package shell
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/c-bata/go-prompt"
+	"golang.org/x/term"
+)
+
+var (
+	termState *term.State
+)
+
+func saveTermState() {
+	oldState, err := term.GetState(int(os.Stdin.Fd()))
+	if err != nil {
+		return
+	}
+	termState = oldState
+}
+
+func restoreTermState() {
+	if termState != nil {
+		term.Restore(int(os.Stdin.Fd()), termState)
+	}
+}
+
+func exit() {
+	restoreTermState()
+	os.Exit(0)
+}
+
+func Run() {
+	defer restoreTermState()
+	saveTermState()
+
+	for {
+		t := prompt.Input(
+			"[picow] ",
+			completer,
+			prompt.OptionPrefixTextColor(prompt.Blue),
+		)
+
+		switch strings.Trim(t, " ") {
+		case "exit", "quit":
+			exit()
+		}
+
+		fmt.Printf("Run command: %s\n", t)
+	}
+}
